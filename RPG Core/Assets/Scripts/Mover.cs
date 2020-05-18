65ae4c43e9c79727de240d99bae36468f6e01b3a
crismand <crismand-dev@outlook.com>
2020-05-18 14:46:47 -0400

    Navigation and Following Camera implemented.

---
+++ RPG Core/Assets/Scripts/Mover.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+
+    [SerializeField] private Transform target;
+
+    private NavMeshAgent _navMeshAgent;
+    private Camera mainCamera;
+
+    private Ray _lastRay;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();                                                    
+        mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+            
+        }
+        //Debug.DrawRay(_lastRay.origin, _lastRay.direction * 100);
+    }
+
+    void MoveToCursor()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hitWorld = Physics.Raycast(ray, out hit);
+
+        if (hitWorld)
+        {
+            _navMeshAgent.destination = hit.point;
+        }
+    }
+}
