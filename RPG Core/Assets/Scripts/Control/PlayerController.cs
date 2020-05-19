ef9df41fd1ed08a1315d332bee4efca710baeb0c
crismand <crismand-dev@outlook.com>
2020-05-19 14:54:34 -0400

    Basic Movement complete Combat System WIP

---
+++ RPG Core/Assets/Scripts/Control/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Movement;
+
+namespace Control
+{
+    public class PlayerController : MonoBehaviour
+    {
+        private Mover _mover;
+        private Camera mainCamera;
+
+        private Ray _lastRay;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _mover = GetComponent<Mover>();
+            if (_mover == null) Debug.LogError("PlayerController cannot find Mover");
+
+            mainCamera = Camera.main;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetMouseButton(0))
+            {
+                MoveToCursor();
+            }
+        }
+
+        void MoveToCursor()
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            bool hitWorld = Physics.Raycast(ray, out hit);
+
+            if (hitWorld)
+            {
+                _mover.MoveTo(hit.point);
+            }
+        }
+    }
+}
+
+
---
+++ RPG Core/Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Movement
+{
+    public class Mover : MonoBehaviour
+    {
+
+        private NavMeshAgent _navMeshAgent;
+        private Animator _animator;
+        
+
+        
+    
+        // Start is called before the first frame update
+        void Start()
+        {
+            _navMeshAgent = GetComponent<NavMeshAgent>();
+            _animator = GetComponent<Animator>();
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            //Debug.DrawRay(_lastRay.origin, _lastRay.direction * 100);
+            UpdateAnimator();
+        }
+
+        
+
+        public void MoveTo(Vector3 destination)
+        {
+            _navMeshAgent.destination = destination;
+        }
+    
+        void UpdateAnimator()
+        {
+            Vector3 velocity = _navMeshAgent.velocity;
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        
+            _animator.SetFloat("ForwardSpeed", localVelocity.z);
+        }
+    }
+}
+
